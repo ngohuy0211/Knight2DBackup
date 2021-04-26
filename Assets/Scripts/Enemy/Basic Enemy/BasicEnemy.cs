0c89d80118d8ab893e65c4d02bbd7aeb38354f13
ngohuy0211 <huyonline456@gmail.com>
2021-04-27 03:40:00 +0700

    add

--- Assets/Scripts/Enemy/Basic Enemy/BasicEnemy.cs
+++ Assets/Scripts/Enemy/Basic Enemy/BasicEnemy.cs
@@ -20,7 +20,7 @@ public class BasicEnemy : MonoBehaviour
         }
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
